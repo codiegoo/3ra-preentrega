f979b1f97d887dba48c771ff1ba109c9c1cb131a
DIEGO FLORES ALCANTAR <diegoedvflores03@gmail.com>
2023-04-12 23:36:02 -0700

    add chat

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,6 +4,7 @@ const routerRealTimeP = require('./routerRealTimeP')
 const routerCarts = require('./routerCarts')
 const productsController = require('../dao/dbManager/controller.products')
 const cartController = require('../dao/dbManager/controller.carts')
+const messageController = require('../dao/dbManager/controller.messages')
 
 const router = app => {
   app.use('/api/products', routerProducts)
@@ -11,6 +12,7 @@ const router = app => {
   app.use('/api/realTimeProducts', routerRealTimeP)
   app.use('/api/dbProducts', productsController)
   app.use('/api/dbCart', cartController)
+  app.use('/api/messages', messageController)
 }
 
 module.exports = router
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,4 +1,5 @@
 const socketIo = require('socket.io')
+const Message = require('../dao/models/Messages.model')
 
 const initSocketServer = httpServer => {
   const io = socketIo(httpServer)
@@ -6,10 +7,24 @@ const initSocketServer = httpServer => {
 
   io.on('connection', socket => {
     console.log('cliente conectado')
+
+    Message.find().then((messages) => {
+      socket.emit('old messages', messages);
+    });
+
+    socket.on('send message', (data) => {
+      const message = new Message({
+        user: data.user,
+        message: data.message
+      });
+      message.save().then(() => {
+        io.emit('new message', message);
+      });
+    });
   })
-  io.on('message', message => {
-    console.log('Mensaje recibido en el servidor:', message)
-  })
+  // io.on('message', message => {
+  //   console.log('Mensaje recibido en el servidor:', message)
+  // })
   return io
 }
 
